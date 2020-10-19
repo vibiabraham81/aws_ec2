d4ab32ef71d4fb1c01f0b019dadd57d3aa841f4a
vibipa <vibipa@gmail.com>
2020-10-18 21:37:22 -0400

    added key value

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "example" {
 ami = var.ami
 instance_type = var.instance_type
-
+key_name = var.key_name
 tags = {
     application    = "size-scaling"
     banner = "banner"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
 variable "region" { default = "us-east-1" }
 variable "instance_type" { default = "t2.micro" }
 variable "ami" { default = "ami-0947d2ba12ee1ff75" }
+variable "key_name" { default = "allocation" }
+
