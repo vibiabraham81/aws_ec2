5507028e4dce917ac7673ab0bbf4de74562995fa
vibipa <vibipa@gmail.com>
2020-10-26 00:37:28 -0400

    Add existing file

--- main.tf
+++ main.tf
@@ -37,3 +37,30 @@ tags = {
   }
 
 }
+resource "aws_instance" "example" {
+ami = var.ami
+instance_type = var.instance_type
+
+
+root_block_device {
+    volume_size = "20"
+  }
+tags = {
+    application    = "size-scaling"
+    banner = "banner"
+    Name   = "size-scaling"
+    projectcode = "CR194003"
+    compliance = "none"
+    owner = "ierra@hbc.com"
+    stakeholder = "reiter@hbc.com"
+    exposure = "internal"
+    projectname = "Enterprise Allocation"
+    workload = "db"
+    availability = "standalone"
+    state = "active"
+    department = "Human Resources"
+    type = "Production"
+  }
+
+}
+
